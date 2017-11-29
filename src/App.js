3a987eeaee700c91f3c48715e0c4ecaad2b9dbba
tomas dornell <tomasdornell@gmail.com>
2017-11-29 12:27:15 +0100

    Created basic structure of the app.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Api from '@parity/api';
 
-class App extends Component {
+const provider = new Api.Provider.Http('https://kovan.infura.io/'); // or Api.Provider.Ws('ws://localhost:8546')
+const api = new Api(provider);
+
+function Details(props) {
+  return (
+    <div>
+      <h3>Address Summary</h3>
+        <ul>
+          <li>Address: {props.address}</li>
+          <li>Account type: </li>
+          <li>Balance: </li>
+          <li>Transaction count: </li>
+          <li><h3>TODO add more</h3></li>
+        </ul>
+    </div>
+  );
+}
+
+class Option1 extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>I'm the first option</h2>
+        <Details address={this.props.address}/>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentAddress: '0x0000000000000000000000000000000000000000',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Option1 address={this.state.currentAddress}/>
       </div>
     );
   }
